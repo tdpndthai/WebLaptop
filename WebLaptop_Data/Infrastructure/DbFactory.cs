1b77e4ee38947caf320bf203881de0a486aa0503
tdpndthai <nguyendanhthai1605@gmail.com>
2019-10-10 23:04:22 +0700

    Complete model and dal

---
+++ WebLaptop_Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLaptop_Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private WebLaptopDbContext dbContext;
+
+        public WebLaptopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new WebLaptopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ WebLaptop_Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLaptop_Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private WebLaptopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public WebLaptopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
